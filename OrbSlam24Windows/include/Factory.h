4281c95d657f1b019fa33be95ed54e222dc0b320
shirhockton <781502127@qq.com>
2021-03-08 10:08:15 +0800

    first commit

---
+++ OrbSlam24Windows/include/Factory.h
@@ -0,0 +1,25 @@
+#ifndef _Factory
+#define _Factory
+
+#include <iostream>
+#include <vector>
+#include <opencv2/opencv.hpp>
+
+// 产品类
+namespace ORB_SLAM2 {
+	class Produce {
+	public:
+		cv::Mat R1;
+		cv::Mat t1;
+		cv::Mat K1;
+		cv::Mat mImRGB;
+		cv::Mat mImGray;
+		int w_images;
+		int h_images;
+		//void 
+	};
+
+	extern std::vector<Produce> ProducePool;
+	extern int ProduceCnt;
+}
+#endif // !_Factory
